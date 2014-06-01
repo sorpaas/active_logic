b4fbb0f20adea1e13ffc68fcf4f903991bd81337
sorpa'as plat <sorpaas@gmail.com>
2014-06-01 19:40:15 +0800

    Use model name in generator template

--- lib/generators/activelogic/install_generator.rb
+++ lib/generators/activelogic/install_generator.rb
@@ -9,8 +9,8 @@ module ActiveLogic
     desc 'ActiveLogic installation generator'
 
     def install
-      model_name = ask_for('What model name do you want to put new declarations?', 'declaration', _model_name)
-      generate 'model', "#{model_name} prefix:string parameters:text"
+      @model_name = ask_for('What model name do you want to put new declarations?', 'declaration', _model_name)
+      generate 'model', "#{@model_name} prefix:string parameters:text"
       template 'initializer.rb', 'config/initializers/activelogic.rb'
     end
   end
