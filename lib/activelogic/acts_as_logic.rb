260873938a05f8c132b09a9351da2615f09e942d
sorpa'as plat <sorpaas@gmail.com>
2014-06-01 19:09:37 +0800

    Add basic template for acts_as_logic

--- lib/activelogic/acts_as_logic.rb
+++ lib/activelogic/acts_as_logic.rb
@@ -1,5 +1,27 @@
 module ActiveLogic
   module ActsAsLogic
+    extend ActiveSupport::Concern
 
+    included do
+
+    end
+
+    module ClassMethods
+      def acts_as_logic(options = {})
+
+      end
+    end
+
+    module LocalInstanceMethods
+      def declare
+
+      end
+
+      def ask
+
+      end
+    end
   end
 end
+
+ActiveRecord::Base.send :include, ActiveLogic::ActsAsLogic
