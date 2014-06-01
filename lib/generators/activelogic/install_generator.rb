049f2be0ae516362d3bf201747c5ff6685f237cb
sorpa'as plat <sorpaas@gmail.com>
2014-06-01 19:23:43 +0800

    init generators

---
+++ lib/generators/activelogic/install_generator.rb
@@ -0,0 +1,17 @@
+require 'rails/generators'
+
+module ActiveLogic
+  class InstallGenerator
+    source_root File.expand_path('../templates', __FILE__)
+    include Generators::Utils::InstanceMethods
+
+    argument :_model_name, type: :string, required: false, desc: 'ActiveLogic declaration model name'
+    desc 'ActiveLogic installation generator'
+
+    def install
+      model_name = ask_for('What model name do you want to put new declarations?', 'declaration', _model_name)
+      generate 'model', "#{model_name} prefix:string parameters:text"
+      template 'initializer.rb', 'config/initializers/activelogic.rb'
+    end
+  end
+end
---
+++ lib/generators/activelogic/templates/initializer.rb
@@ -0,0 +1,4 @@
+ActiveLogic.config do |config|
+  # Logic declaration model name
+  config.model_name :declaration
+end
