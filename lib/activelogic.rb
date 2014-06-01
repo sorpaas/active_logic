29b53df048d32abb01eb9cde8670a69938df1ab6
sorpa'as plat <sorpaas@gmail.com>
2014-06-01 19:34:31 +0800

    Add configuration function

--- lib/activelogic.rb
+++ lib/activelogic.rb
@@ -2,4 +2,11 @@ require 'activelogic/version'
 require 'activelogic/acts_as_logic'
 
 module ActiveLogic
+  def self.config(&block)
+    if block_given?
+      block.call(RailsAdmin::Config)
+    else
+      RailsAdmin::Config
+    end
+  end
 end
---
+++ lib/activelogic/config.rb
@@ -0,0 +1,17 @@
+require 'active_support/core_ext/module/attribute_accessors'
+
+module ActiveLogic
+  module Config
+    DEFAULT_MODEL_NAME = :declaration
+
+    class << self
+      attr_accessor :model_name
+
+      def reset
+        @model_name = DEFAULT_MODEL_NAME
+      end
+
+      reset
+    end
+  end
+end
